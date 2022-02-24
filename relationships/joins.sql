6560b659589d41c9feac8dbb5a03702ab9fb2660
tron135 <myash180@gmail.com>
2022-02-24 16:45:45 +0000

    inner and left join done

---
+++ relationships/joins.sql
@@ -0,0 +1,19 @@
+-- select first_name, last_name, order_date, amount
+-- from customers
+-- join orders
+--     on customers.id = orders.customer_id;
+
+-- select first_name, last_name, order_date, amount
+-- from orders
+-- join customers
+--     on customers.id = orders.customer_id;
+
+select 
+    first_name, 
+    last_name, 
+    ifnull(sum(amount), 0) as total_spent
+from customers
+left join orders
+    on customers.id = orders.customer_id
+group by customers.id
+order by total_spent;
